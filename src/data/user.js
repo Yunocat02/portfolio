d3064b676ed05d0b3849aa998950c1d9dc422392
Yunocat02 <s6303051623080@email.kmutnb.ac.th>
2023-10-21 13:58:44 +0700

    first commit

---
+++ src/data/user.js
@@ -0,0 +1,57 @@
+const INFO = {
+	main: {
+		title: "Reactfolio",
+		name: "Chawanwit Kaenkong",
+		email: "yunocat.chai@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/cat_yuno27754",
+		github: "https://github.com/Yunocat02",
+		linkedin: "https://www.linkedin.com/in/yuno-cat-520850297/",
+		instagram: "https://www.instagram.com/chai_yuno/",
+		stackoverflow: "https://stackoverflow.com/users/22778262/yunocat",
+		facebook: "https://www.facebook.com/ChaiKunG.02",
+	},
+
+	homepage: {
+		title: "Full-stack web and mobile app developer, and 3D Model.",
+		description:
+			"I'm a versatile developer with expertise in web and mobile app development, as well as Arduino robotics. I've thrived in coding competitions, honed 3D modeling skills with Blender and Unity, and I'm ready to bring your ideas to life!",
+	},
+
+	about: {
+		title: "I’m Chai I live in Thailand",
+		description:
+			"As a recent graduate with a burning passion for programming, I find myself at the threshold of an exciting journey. The transition from academia to the professional world can be a challenging but immensely rewarding experience. In this article, I'll share my early insights, aspirations, and the path I'm charting as a budding programmer.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"As a recent graduate with a burning passion for programming, I find myself at the threshold of an exciting journey. The transition from academia to the professional world can be a challenging but immensely rewarding experience. In this article, I'll share my early insights, aspirations, and the path I'm charting as a budding programmer.",
+	},
+
+	projects: [
+		{
+			title: "LittleBuddy",
+			description:
+				"An application that enables customers to search for nearby clinics, offering a chatbot for basic inquiries, allows registered users to access treatment details, view in-clinic webcam feeds, and provides a private in-app chat feature.",
+			logo: "https://storage.googleapis.com/cms-storage-bucket/0dbfcc7a59cd1cf16282.png",
+			linkText: "View Project",
+			link: "https://github.com/Yunocat02/LittleBuddy",
+		},
+
+		{
+			title: "Keptang",
+			description:
+				"A web platform designed to summarize and store customer income and expenses data, with the backend programmed in the Rust language.",
+			logo: "https://www.rust-lang.org/logos/rust-logo-128x128-blk.png",
+			linkText: "View Project",
+			link: "https://github.com/Yunocat02/Keptang",
+		},
+	],
+};
+
+export default INFO;
